76e67c0a0bcc3f0e05d877226d4aaa92f1e52bb4
Fábio Correia <fabio.correia@moloni.com>
2022-06-20 10:12:51 +0100

    Copyright

--- src/Exceptions/Document/MoloniDocumentCustomerException.php
+++ src/Exceptions/Document/MoloniDocumentCustomerException.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * 2022 - Moloni.com
+ *
+ * NOTICE OF LICENSE
+ *
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Moloni
+ * @copyright Moloni
+ * @license   https://creativecommons.org/licenses/by-nd/4.0/
+ *
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ */
 
 namespace Moloni\Exceptions\Document;
 
