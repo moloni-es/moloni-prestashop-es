c3c425aa5b411fd1226b7c6d284cb7a21cb9a9f8
Fábio Correia <fabio.correia@moloni.com>
2022-06-20 10:12:51 +0100

    Copyright

--- src/Form/Settings/SettingsFormType.php
+++ src/Form/Settings/SettingsFormType.php
@@ -1,5 +1,28 @@
 <?php
-/** @noinspection PhpReturnValueOfMethodIsNeverUsedInspection */
+/**
+ * 2022 - Moloni.com
+ *
+ * NOTICE OF LICENSE
+ *
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Moloni
+ * @copyright Moloni
+ * @license   https://creativecommons.org/licenses/by-nd/4.0/
+ *
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpReturnValueOfMethodIsNeverUsedInspection
+ */
+
 
 declare(strict_types=1);
 
