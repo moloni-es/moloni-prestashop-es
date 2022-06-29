b51c30489993938cff383ff77279cc359fb6c5c5
Fábio Correia <fabio.correia@moloni.com>
2022-06-29 09:22:08 +0100

    Moloni product - New before update verification

--- src/Builders/MoloniProductWithVariants.php
+++ src/Builders/MoloniProductWithVariants.php
@@ -367,7 +367,7 @@ class MoloniProductWithVariants implements BuilderInterface
      */
     protected function beforeUpdate(): void
     {
-        if (empty($this->moloniProduct['variants']) && $this->productExists()) {
+        if (!$this->moloniProduct['deletable'] && empty($this->moloniProduct['variants']) && $this->productExists()) {
             throw new MoloniProductException('Cannot update product in Moloni. Product types do not match');
         }
     }
