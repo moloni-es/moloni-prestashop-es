2b2548f1f16887e52448046b383c066342120fb9
Fábio Correia <fabio.correia@moloni.com>
2022-07-01 10:10:54 +0100

    Update faqs url

--- src/Enums/Domains.php
+++ src/Enums/Domains.php
@@ -27,7 +27,7 @@ namespace Moloni\Enums;
 class Domains
 {
     public const MOLONI = 'https://moloni.es';
-    public const MOLONI_FAQS = 'https://moloni.es/faqs/category/plugins-e-integraciones';
+    public const MOLONI_FAQS = 'https://www.moloni.es/faqs/subcategory/prestashop';
     public const MOLONI_AC = 'https://ac.moloni.es';
     public const MOLONI_API = 'https://api.moloni.es/v1';
     public const MOLONI_MEDIA_API = 'https://mediaapi.moloni.org';
